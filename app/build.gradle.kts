319e033258904d42194b3d51aebf102fc398ea6f
axiel7 <axiel7lopez@gmail.com>
2023-08-24 14:08:13 +0900

    target sdk 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.axiel7.tachisync"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.axiel7.tachisync"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 5
         versionName = "1.0.4"
 
