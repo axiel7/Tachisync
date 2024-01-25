61f41436b9aedb1f525ec56f2f420f890be9cf84
axiel7 <axiel7lopez@gmail.com>
2024-01-25 13:44:58 +0900

    update libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,13 +69,13 @@ dependencies {
     implementation("androidx.documentfile:documentfile:1.0.1")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.01.00"))
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.material3:material3-android:1.2.0-beta01")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.compose.material3:material3-android:1.2.0-rc01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.navigation:navigation-compose:2.7.6")
 
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7")
@@ -84,5 +84,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.0")
 }
