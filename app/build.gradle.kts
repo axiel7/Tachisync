c1abf2753de97a074225fc6925af5866b9c9d510
axiel7 <axiel7lopez@gmail.com>
2024-04-18 15:50:22 +0200

    update compose bom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     implementation("androidx.documentfile:documentfile:1.0.1")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
@@ -87,5 +87,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.5")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
