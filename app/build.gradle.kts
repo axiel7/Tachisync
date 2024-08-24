3d29ffe1f1669239dd8e7efa921748b9d3363631
axiel7 <axiel7lopez@gmail.com>
2024-08-24 11:30:09 +0200

    chore: upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,13 +69,13 @@ dependencies {
     implementation("androidx.documentfile:documentfile:1.0.1")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.08.00"))
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
-    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.activity:activity-compose:1.9.1")
     implementation("androidx.compose.material3:material3-android:1.2.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7")
