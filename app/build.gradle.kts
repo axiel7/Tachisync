d8784f039594c0e93799503cabb3150bcf0096bf
axiel7 <axiel7lopez@gmail.com>
2024-08-24 11:36:23 +0200

    build: 1.0.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.axiel7.tachisync"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.9"
+        versionCode = 11
+        versionName = "1.0.10"
         setProperty("archivesBaseName", "tachisync-$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
